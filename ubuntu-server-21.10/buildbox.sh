01d5e31ba7157a98706a4ede91a7eadd8d9b8c22
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-01-08 17:36:02 +0000

    Update buildbox.sh

--- ubuntu-server-21.10/buildbox.sh
+++ ubuntu-server-21.10/buildbox.sh
@@ -3,6 +3,7 @@
 packer init vmware-impish.pkr.hcl
 packer build -force vmware-impish.pkr.hcl
 cp metadata.json output/metadata.json
-tar cvzf output/vmware-impish-arm64.box output/*
-md5 output/vmware-impish-arm64.box
-rm -f output/*.v* output/*.nvram output/metadata.json
+cd output
+tar cvzf vmware-impish-arm64.box ./*
+md5 vmware-impish-arm64.box
+rm -f *.v* *.nvram metadata.json
