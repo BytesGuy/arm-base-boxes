3a4a0e7fe4400f9d6e8d96863223fccf7be15f72
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-04-16 21:58:00 +0100

    Update parallels-cleanup.sh

--- debian-11/parallels-cleanup.sh
+++ debian-11/parallels-cleanup.sh
@@ -6,9 +6,6 @@ sudo /media/tools/install --install-unattended-with-deps
 sudo umount /media/tools
 sudo rm -f /tmp/prltools.iso
 
-umount /dev/dvd
-rm ~/*iso
-
 sudo apt-get clean
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
