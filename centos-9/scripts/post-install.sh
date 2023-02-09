0675fec88bf6951d26b25b204b50bbfb20f0e2eb
dbrennand <52419383+dbrennand@users.noreply.github.com>
2023-02-09 18:06:04 +0000

    refactor: remove drag as centos default FS is xfs

--- centos-9/scripts/post-install.sh
+++ centos-9/scripts/post-install.sh
@@ -6,8 +6,7 @@ chmod 0700 /home/vagrant/.ssh
 wget -O /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 
-# Defrag and shrink the disk
-sudo e4defrag /
+# Shrink the disk
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
 sync
