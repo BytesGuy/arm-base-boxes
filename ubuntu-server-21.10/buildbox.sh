9e23eddc501e46b3ec635bc678aca22de82b34db
Adam Hartley <BytesGuy@users.noreply.github.com>
2025-03-24 11:16:22 +0000

    Remove Ubuntu Server 21.10 due to EOL

--- ubuntu-server-21.10/buildbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-packer init vmware-impish.pkr.hcl
-packer build -force vmware-impish.pkr.hcl
-cp metadata.json output/metadata.json
-cd output
-tar cvzf vmware-impish-arm64.box ./*
-md5 vmware-impish-arm64.box
-rm -f *.v* *.nvram metadata.json
